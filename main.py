7a53d9aa4d9ec291c6c23e0901c3f832371cf346
uvaprol <120366489+uvaprol@users.noreply.github.com>
2025-03-27 19:04:58 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,93 +1,46 @@
 import pygame
-from time import sleep
+from game import *
 pygame.init()
-w = 610 # ширина экрана
-h = 610 # высота экрана
-display = pygame.display.set_mode((w, h))
 
-white = (255, 255, 255)
-red = (179, 55, 55)
-green = (47, 99, 64)
-black = (0, 0, 0)
-blue = (54, 156, 179)
+W = 610
+H = 610
 
-field = [['' for _ in range(3)] for _ in range(3)]
-X_flag = True
+WHITE = (255, 255, 255)
+RED   = (179, 55, 55)
+GREEN = (47, 99, 64)
+BLACK = (0, 0, 0)
+BLUE  = (54, 156, 179)
+
 message = ''
-game_over = False
+
+display = pygame.display.set_mode((W, H))
 
 def render():
-    display.fill(white)
-    pygame.draw.line(display, black, (200, 0), (200, h), 5)
-    pygame.draw.line(display, black, (405, 0), (405, h), 5)
-    pygame.draw.line(display, black, (0, 200), (h, 200), 5)
-    pygame.draw.line(display, black, (0, 405), (h, 405), 5)
+    display.fill(WHITE)
+    pygame.draw.line(display, BLACK, (200, 0), (200, H), 5)
+    pygame.draw.line(display, BLACK, (405, 0), (405, H), 5)
+    pygame.draw.line(display, BLACK, (0, 200), (H, 200), 5)
+    pygame.draw.line(display, BLACK, (0, 405), (H, 405), 5)
     for y in range(3):
         for x in range(3):
-            if field[y][x] == 'x':
-                display.blit(pygame.font.SysFont("None", 300).render('x', True, red), [45 + 200 * x, 0 + 200 * y])
-            elif field[y][x] == 'o':
-                display.blit(pygame.font.SysFont("None", 300).render('o', True, blue), [45 + 200 * x, 0 + 200 * y])
-    display.blit(pygame.font.SysFont("None", 50).render(message, True, green), [295, 295])
+            display.blit(pygame.font.SysFont("None", 300).render(f'{fill[y][x]}', True, RED if fill[y][x] == 'x' else BLUE), [45 + 200 * x, 0 + 200 * y])
+    display.blit(pygame.font.SysFont("None", 50).render(message, True, GREEN), [295, 295])
     pygame.display.flip()
-
-def check_game_over():
-    global game_over
-    global message
-    for line in field:
-        if line == ['x', 'x', 'x'] or line == ['o', 'o', 'o']:
-            message = 'You win!'
-            game_over = True
-            return
-    for colum in range(3):
-        if field[0][colum] == field[1][colum] == field[2][colum] == 'x' or field[0][colum] == field[1][colum] == field[2][colum] == 'o':
-            message = 'You win!'
-            game_over = True
-            return
-    if field[0][0] == field[1][1] == field[2][2] == 'x' or field[0][1] == field[1][1] == field[2][0] == 'x' or field[0][0] == field[1][1] == field[2][2] == 'o' or field[0][1] == field[1][1] == field[2][0] == 'o':
-        message = 'You win!'
-        game_over = True
-        return
-
-    for line in field:
-        if not '' in line:
-            game_over = True
-        else:
-            game_over = False
-            break
-    if game_over:
-        message = 'Draw'
-        game_over = True
-
-def user_step(x, y):
-    global X_flag
-    global field
-    if field[y][x] == '':
-        if X_flag:
-            field[y][x] = 'x'
-            X_flag = False
-        else:
-            field[y][x] = 'o'
-            X_flag = True
 
 render()
 while not game_over:
-    display.fill(white)
     events = pygame.event.get()
     for event in events:
-        # print(event)
+        print(event)
         if event.type == pygame.QUIT:
             pygame.quit()
             quit()
-        elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_ESCAPE:
-                pygame.quit()
-                quit()
+        elif event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+            pygame.quit()
+            quit()
         elif event.type == pygame.MOUSEBUTTONDOWN:
             x, y = pygame.mouse.get_pos()
             x, y = x // 205, y // 205
-            user_step(x, y)
-            check_game_over()
-            render()
-
-sleep(5)
+            if player_step(x, y):
+                message = win_check()
+                render()
